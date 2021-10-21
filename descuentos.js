290f18030c0503d3fea46455ade3732dfb39814a
Nico <nicolasimplant@gmail.com>
2021-10-21 08:42:20 -0500

    Modificado funcional cupones

--- descuentos.js
+++ descuentos.js
@@ -3,8 +3,18 @@
 //var procentajePrecioConDescuento = (1-descuento);
 //var precioConDescuento = (precioOriginal*procentajePrecioConDescuento);
 
-function calcularPrecioConDescuento(precio,descuento){
-    var descuento = descuento/100;
+function calcularPrecioConDescuento(precio,descuento,cupon){
+    var bono = cupon;
+    var cupones = [
+        {tipo:"Bono_De_Regalo", decuento: 0.15},
+        {tipo:"Cliente_Frecuente", decuento: 0.30},
+        {tipo: "Cliente_Premiun", decuento: 0.40},
+        {tipo: "ninguno", decuento: 0}
+    ]
+    var descuentoCupon = cupones.find(function(cupon){
+        return cupon.tipo === bono;
+    });
+    var descuento = descuento/100 + descuentoCupon.decuento;
     var procentajePrecioConDescuento = (1-descuento);
     var precioConDescuento = (precio*procentajePrecioConDescuento);
     return precioConDescuento
@@ -14,17 +24,14 @@ function calcularPrecioConDescuento(precio,descuento){
 function onclickButtonPriceDiscount(){
     var inputPrice = parseFloat(document.getElementById("InputPrice").value);
     var inputDiscount = parseFloat(document.getElementById("InputDiscount").value);
-    var precioReal = calcularPrecioConDescuento(inputPrice,inputDiscount);
-    var resultP = document.getElementById("ResultPrice")
+    var cupon = document.getElementById("Bonus").value;
+    var precioReal = calcularPrecioConDescuento(inputPrice,inputDiscount,cupon);
+
+    var resultP = document.getElementById("ResultPrice")    
     resultP.innerText = "El precio con decuento son: $" + precioReal;
 }
 
-var cupones = [
-    {tipo:"Bono_De_Regalo", decuento: 0.15},
-    {tipo:"Cliente_Frecuente", decuento: 0.30},
-    {tipo: "Cliente_Premiun", decuento: 0.40},
-    {tipo: "ninguno", decuento: 0}
-]
+
 
 
 
